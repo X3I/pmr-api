76bb755064b72655264dee06f7e000b6ded7369e
jakemadness <j4kem4dness@live.com>
2018-07-09 09:10:35 +0100

    Update poke.api.js

--- _/poke.api.js
+++ _/poke.api.js
@@ -1,5 +1,44 @@
 window.pokeApi = function(utilities, socket) {
    var self      = this;
+   self.register = function(username, password, email, pokemonId, success, error) {
+      utilities.postRequest('/ajax/register', utilities.queryString({
+         'subscribe': 'on',
+         'username':  username,
+         'password1': password,
+         'password2': password,
+         'email':     email,
+         'monster':   pokemonId
+      }), function(data) {
+         ((data = JSON.parse(data)) && 'result' in data && data.result ? (success && success()) : (error && error()));
+      });
+   };
+   self.activate = function(gender, body, hair, shirt, pants, shoes, token, success, error) {
+      utilities.postRequest('/ajax/account/complete', utilities.queryString({
+         'gender': gender,
+         'body':   body,
+         'hair':   hair,
+         'shirt':  shirt,
+         'pants':  pants,
+         'shoes':  shoes,
+         'token':  token
+      }), function(data) {
+         ((data = JSON.parse(data)) && 'result' in data && data.result ? (success && success()) : (error && error()));
+      });
+   };
+   self.login = function(username, password, success, error) {
+      utilities.postRequest('/', utilities.queryString({
+         'username': username,
+         'password': password
+      }), function(data) {
+         ((match = data.match(/token\s*=\s*.([0-9a-z]*)/i)) ? (success && success(match['1'])) : (error && error()));
+      });
+   };
+   self.authenticate = function(token, callback) {
+      socket.send({'a': 'auth', 'p': {'token': token}}), function(data) {
+         console.log('authenticated!');
+         callback(data);
+      });
+   };
    self.useEmote = function(emote) {
       socket.send({'a': 'emote', 'p': {'style': emote}});
    };
@@ -35,38 +74,5 @@ window.pokeApi = function(utilities, socket) {
    };
    self.speakToNpc = function(npc) {
       socket.send({'a': 'action', 'p': {'npc': npc}});
-   };
-   self.register = function(username, password, email, pokemonId, success, error) {
-      utilities.postRequest('/ajax/register', utilities.queryString({
-         'subscribe': 'on',
-         'username':  username,
-         'password1': password,
-         'password2': password,
-         'email':     email,
-         'monster':   pokemonId
-      }), function(data) {
-         ((data = JSON.parse(data)) && 'result' in data && data.result ? (success && success()) : (error && error()));
-      });
-   };
-   self.activate = function(gender, body, hair, shirt, pants, shoes, token, success, error) {
-      utilities.postRequest('/ajax/account/complete', utilities.queryString({
-         'gender': gender,
-         'body':   body,
-         'hair':   hair,
-         'shirt':  shirt,
-         'pants':  pants,
-         'shoes':  shoes,
-         'token':  token
-      }), function(data) {
-         ((data = JSON.parse(data)) && 'result' in data && data.result ? (success && success()) : (error && error()));
-      });
-   };
-   self.login = function(username, password, success, error) {
-      utilities.postRequest('/', utilities.queryString({
-         'username': username,
-         'password': password
-      }), function(data) {
-         ((match = data.match(/token\s*=\s*.([0-9a-z]*)/i)) ? (success && success(match['1'])) : (error && error()));
-      });
    };
 };
