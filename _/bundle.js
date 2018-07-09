86bb1aee37f88d1afd87d9643a2aa2c08f67514d
jakemadness <j4kem4dness@live.com>
2018-07-09 04:41:16 +0100

    Update bundle.js

--- _/bundle.js
+++ _/bundle.js
@@ -37,64 +37,55 @@
    /*
    * SOCKET | private
    */
-   var socket = function() {
-      var self          = this;
-      self.ws           = false;
-      self.connected    = false;
-      self.receiveCount = 0;
-      self.sendCount    = 0;
-      self.open         = function(proto, host, openCallback, closeCallback, receiveCallback) {
-         self.ws        = new WebSocket(proto + '://' + host);
+   var socket = function(proto, host) {
+      var self       = this;
+      self.proto     = proto;
+      self.host      = host;
+      self.ws        = false;
+      self.connected = false;
+      self.sentCount = 0;
+      self.callbacks = {};
+      self.open      = function() {
+         self.ws = new WebSocket(self.proto + '://' + self.host);
+      };
+      self.setEvents = function() {
          self.ws.onopen = function() {
             self.connected = true;
-            (openCallback && openCallback());
          };
          self.ws.onclose = function() {
             self.connected = false;
-            (closeCallback && closeCallback());
          };
          self.ws.onmessage = function(event) {
-            ++self.receiveCount;
-            (receiveCallback && receiveCallback(event.data));
+            var data = JSON.parse(event.data);
+            if ( data.id in self.callbacks ) {
+               self.callbacks[data.id](data);
+               delete self.callbacks[data.id];
+            }
+            else {
+               console.log(data);
+            }
          };
       };
-      self.close = function() {
-         self.ws.close();
+      self.send = function(packet, callback) {
+         packet.id = 'P' + self.sentCount;
+         self.ws.send(JSON.stringify(packet));
+         ++self.sentCount;
+         if ( callback ) {
+            self.callbacks[packet.id] = callback;
+         }
       };
-      self.send = function(data) {
-         ++self.sendCount;
-         self.ws.send(data);
-      };
+      self.open();
+      self.setEvents();
    };
 
 
    /*
    * API | public
    */
-   window.api = function() {
-      var self        = this;
-      self.utilities  = new utilities();
-      self.socket     = new socket();
-      self.callbacks  = {};
-      self.openSocket = function(proto, host, callback) {
-         self.socket.open(proto, host, callback, false, function(data) {
-            data = JSON.parse(data);
-            if ( data.id in self.callbacks ) {
-               self.callbacks[data.id](data);
-               delete self.callbacks[data.id];
-            }
-            else {
-               console.log('unknown packet');
-            }
-         });
-      };
-      self.sendPacket = function(packet, callback) {
-         packet.id = 'P' + self.socket.sendCount;
-         self.socket.send(JSON.stringify(packet));
-         if ( callback ) {
-            self.callbacks[packet.id] = callback;
-         }
-      };
+   window.api = function(proto, host) {
+      var self       = this;
+      self.utilities = new utilities();
+      self.socket    = new socket(proto, host);
       self.register  = function(username, password, email, pokemonId, success, error) {
          self.utilities.postRequest('/ajax/register', self.utilities.queryString({
             'subscribe': 'on',
