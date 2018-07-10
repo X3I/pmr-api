688bcd8c5f73799d7363df08e316028699de180e
jakemadness <j4kem4dness@live.com>
2018-07-10 09:48:56 +0100

    Update poke.api.js

--- poke.api.js
+++ poke.api.js
@@ -1,7 +1,10 @@
 window.pokeApi = function(utilities, data, socket) {
-   var self      = this;
+   var self       = this;
+   self.utilities = utilities;
+   self.data      = data;
+   self.socket    = socket;
    self.register = function(username, password, email, pokemonId, success, error) {
-      utilities.postRequest('/ajax/register', utilities.queryString({
+      self.utilities.postRequest('/ajax/register', self.utilities.queryString({
          'subscribe': 'on',
          'username':  username,
          'password1': password,
@@ -13,7 +16,7 @@ window.pokeApi = function(utilities, data, socket) {
       });
    };
    self.activate = function(gender, body, hair, shirt, pants, shoes, token, success, error) {
-      utilities.postRequest('/ajax/account/complete', utilities.queryString({
+      self.utilities.postRequest('/ajax/account/complete', self.utilities.queryString({
          'gender': gender,
          'body':   body,
          'hair':   hair,
@@ -26,7 +29,7 @@ window.pokeApi = function(utilities, data, socket) {
       });
    };
    self.login = function(username, password, success, error) {
-      utilities.postRequest('/', utilities.queryString({
+      self.utilities.postRequest('/', self.utilities.queryString({
          'username': username,
          'password': password
       }), function(data) {
@@ -34,75 +37,75 @@ window.pokeApi = function(utilities, data, socket) {
       });
    };
    self.authenticate = function(token, callback) {
-      socket.send({'a': 'auth', 'p': {'token': token}}, callback);
+      self.socket.send({'a': 'auth', 'p': {'token': token}}, callback);
    };
    self.useEmote = function(emote) {
-      socket.send({'a': 'emote', 'p': {'style': emote}});
+      self.socket.send({'a': 'emote', 'p': {'style': emote}});
    };
    self.moveAvatar = function(x, y) {
-      socket.send({'a': 'target', 'direct': 0, 'p': {'x': x, 'y': y}});
+      self.socket.send({'a': 'target', 'direct': 0, 'p': {'x': x, 'y': y}});
    };
    self.addFriend = function(username) {
-      socket.send({'a': 'friend_request', 'p': {'name': username}});
+      self.socket.send({'a': 'friend_request', 'p': {'name': username}});
    };
    self.privateMessage = function(username, text) {
-      socket.send({'a': 'message', 'p': {'chatroom': '1', 'text': '/pm' + ' ' + username + ' ' + text}});
+      self.socket.send({'a': 'message', 'p': {'chatroom': '1', 'text': '/pm' + ' ' + username + ' ' + text}});
    };
    self.globalMessage = function(text) {
-      socket.send({'a': 'message', 'p': {'chatroom': '1', 'text': text}});
+      self.socket.send({'a': 'message', 'p': {'chatroom': '1', 'text': text}});
    };
    self.guildMessage = function(text) {
-      socket.send({'a': 'message', 'p': {'chatroom': 'P', 'text': text}});
+      self.socket.send({'a': 'message', 'p': {'chatroom': 'P', 'text': text}});
    };
    self.tradeRequest = function(username) {
-      socket.send({'a': 'trade_request', 'p': {'name': username}});
+      self.socket.send({'a': 'trade_request', 'p': {'name': username}});
    };
    self.tradeStart = function(userId) {
-      socket.send({'a': 'trade_start', 'p': {'with': userId}});
+      self.socket.send({'a': 'trade_start', 'p': {'with': userId}});
    };
    self.tradeMoney = function(amount) {
-      socket.send({'a': 'trade_update', 'p': {'action': 'money', 'money': amount}});
+      self.socket.send({'a': 'trade_update', 'p': {'action': 'money', 'money': amount}});
    };
    self.tradePokemon = function(id) {
-      socket.send({'a': 'trade_update', 'p': {'action': 'pokemon_add', 'pk': id}});
+      self.socket.send({'a': 'trade_update', 'p': {'action': 'pokemon_add', 'pk': id}});
    };
    self.tradeItem = function(id) {
-      socket.send({'a': 'trade_update', 'p': {'action': 'item_add', 'pk': id}});
+      self.socket.send({'a': 'trade_update', 'p': {'action': 'item_add', 'pk': id}});
    };
    self.lockTrade = function() {
-      socket.send({'a': 'trade_update', 'p': {'action': 'lock'}});
+      self.socket.send({'a': 'trade_update', 'p': {'action': 'lock'}});
    };
    self.acceptTrade = function() {
-      socket.send({'a': 'trade_update', 'p': {'action': 'accept'}});
+      self.socket.send({'a': 'trade_update', 'p': {'action': 'accept'}});
    };
    self.speakToNpc = function(npc) {
-      socket.send({'a': 'action', 'p': {'npc': npc}});
+      self.socket.send({'a': 'action', 'p': {'npc': npc}});
    };
    self.useMasterBall = function(target) {
-      socket.send({'a': 'pokeball', 'p': {'t': target, 'i': 1}});
+      self.socket.send({'a': 'pokeball', 'p': {'t': target, 'i': 1}});
    };
    self.useUltraBall = function(target) {
-      socket.send({'a': 'pokeball', 'p': {'t': target, 'i': 2}});
+      self.socket.send({'a': 'pokeball', 'p': {'t': target, 'i': 2}});
    };
    self.useGreatBall = function(target) {
-      socket.send({'a': 'pokeball', 'p': {'t': target, 'i': 3}});
+      self.socket.send({'a': 'pokeball', 'p': {'t': target, 'i': 3}});
    };
    self.usePokeBall = function(target) {
-      socket.send({'a': 'pokeball', 'p': {'t': target, 'i': 4}});
+      self.socket.send({'a': 'pokeball', 'p': {'t': target, 'i': 4}});
    };
    self.withdrawPokemon = function(position, id) {
-      socket.send({'a': 'call', 'p': {'position': position, 'pk': id, 'action': 'withdraw'}});
+      self.socket.send({'a': 'call', 'p': {'position': position, 'pk': id, 'action': 'withdraw'}});
    };
    self.sendoutPokemon = function(position, id) {
-      socket.send({'a': 'call', 'p': {'position': position, 'pk': id, 'action': 'sendout'}});
+      self.socket.send({'a': 'call', 'p': {'position': position, 'pk': id, 'action': 'sendout'}});
    };
    self.evolvePokemon = function(id, from, to) {
-      socket.send({'a': 'evo', 'p': {'pk': id, 'from': from, 'to': to}});
+      self.socket.send({'a': 'evo', 'p': {'pk': id, 'from': from, 'to': to}});
    };
    self.changeSkill = function(id, index) {
-      socket.send({'a': 'skill', 'p': {'pk': id, 'index': index}});
+      self.socket.send({'a': 'skill', 'p': {'pk': id, 'index': index}});
    };
    self.dumpData = function() {
-      console.log(data);
+      console.log(self.data);
    };
 };
