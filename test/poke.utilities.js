c9c003f0a763be769a3e855cbf2cf3249eeda49a
jakemadness <j4kem4dness@live.com>
2018-07-11 00:37:00 +0100

    Create poke.utilities.js

---
+++ test/poke.utilities.js
@@ -0,0 +1,38 @@
+window.pokeUtilities = function() {
+   var self          = this;
+   self.keysInObject = function(object, keys) {
+      for ( var i = 0; i < keys.length; i++ ) {
+         if ( !(keys[i] in object) ) {
+            return false;
+         }
+      }
+      return true;
+   };
+   self.findBy = function(array, key, value) {
+      for ( var i = 0; i < array.length; i++ ) {
+         if ( array[i][key] == value ) {
+            return array[i];
+         }
+      }
+   };
+   self.deleteBy = function(array, key, value) {
+      for ( var i = 0; i < array.length; i++ ) {
+         if ( array[i][key] == value ) {
+            array.splice(i, 1);
+            break;
+         }
+      }
+   };
+   self.interceptSocket = function(url, set, parse) {
+      var websocket    = window.WebSocket;
+      window.WebSocket = function(arg1, arg2) {
+         var ws = new websocket(arg1 || '', arg2 || '');
+         (url == arg1 && set(ws));
+         ws.addEventListener('message', function(event) {
+            (url == this.url && parse(JSON.parse(event.data)));
+         });
+         return ws;
+      };
+      window.WebSocket.prototype = websocket.prototype;
+   };
+};
