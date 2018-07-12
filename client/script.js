0d127d8b8aeeef6cf289048aa76da4ed782aaa5f
jakemadness <j4kem4dness@live.com>
2018-07-12 13:40:09 +0100

    Update script.js

--- client/script.js
+++ client/script.js
@@ -1,6 +1,7 @@
 window.pokeScript = function(server, modifications) {
    var utilities = new window.pokeUtilities();
    var data      = new window.pokeData();
-   var api       = new window.pokeApi(server, utilities, data);
+   var parser    = new pokeParser(utilities, data);
+   var api       = new window.pokeApi(server, utilities, data, parser);
    modifications(utilities, data, api);
 };
