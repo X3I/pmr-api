4324ec1d5558b528639638354a440f3f06e4cc3e
jakemadness <j4kem4dness@live.com>
2018-07-12 16:10:51 +0100

    Update poke.api.js

--- client/poke.api.js
+++ client/poke.api.js
@@ -19,26 +19,14 @@ window.pokeApi = function(server, utilities, data, parser) {
    self.addFriend = function(username) {
       self.sendPacket('friend_request', {'name': username});
    };
-   self.tradeRequest = function(username) {
-      self.sendPacket('trade_request', {'name': username});
+   self.guildInvite = function(username) {
+      self.sendPacket('guildInvite', {'name': username});
    };
-   self.tradeStart = function(userId) {
-      self.sendPacket('trade_start', {'with': userId});
+   self.guildUpdate = function(id, name, description, color) {
+      self.sendPacket('guildUpdate', {'guild_id': id, 'guild_name': name, 'guild_description': description, 'guild_color': color});
    };
-   self.tradeMoney = function(amount) {
-      self.sendPacket('trade_update', {'action': 'money', 'money': amount});
-   };
-   self.tradePokemon = function(id) {
-      self.sendPacket('trade_update', {'action': 'pokemon_add', 'pk': id});
-   };
-   self.tradeItem = function(id) {
-      self.sendPacket('trade_update', {'action': 'item_add', 'pk': id});
-   };
-   self.lockTrade = function() {
-      self.sendPacket('trade_update', {'action': 'lock'});
-   };
-   self.acceptTrade = function() {
-      self.sendPacket('trade_update', {'action': 'accept'});
+   self.guildMoney = function(money) {
+      self.sendPacket('guildTransferMoney', {'money': money});
    };
    self.privateMessage = function(username, text) {
       self.sendPacket('message', {'chatroom': '1', 'text': '/pm' + ' ' + username + ' ' + text});
@@ -49,30 +37,28 @@ window.pokeApi = function(server, utilities, data, parser) {
    self.guildMessage = function(text) {
       self.sendPacket('message', {'chatroom': 'P', 'text': text});
    };
-   self.guildInvite = function(username) {
-      self.sendPacket('guildInvite', {'name': username});
+   self.tradeRequest = function(username) {
+      self.sendPacket('trade_request', {'name': username});
    };
-   self.guildUpdate = function(id, name, description, color) {
-      self.sendPacket('guildUpdate', {'guild_id': id, 'guild_name': name, 'guild_description': description, 'guild_color': color});
+   self.tradeStart = function(userId) {
+      self.sendPacket('trade_start', {'with': userId});
    };
-   self.guildMoney = function(money) {
-      self.sendPacket('guildTransferMoney', {'money': money});
+   self.tradeMoney = function(amount) {
+      self.sendPacket('trade_update', {'action': 'money', 'money': amount});
    };
-   self.useMasterBall = function(target) {
-      var item = utilities.findBy(data.itemList, 'name', 'Master Ball');
-      (item && item.quantity > 0 && self.sendPacket('pokeball', {'t': target, 'i': item.id}));
+   self.lockTrade = function() {
+      self.sendPacket('trade_update', {'action': 'lock'});
    };
-   self.useUltraBall = function(target) {
-      var item = utilities.findBy(data.itemList, 'name', 'Ultra Ball');
-      (item && item.quantity > 0 && self.sendPacket('pokeball', {'t': target, 'i': item.id}));
+   self.acceptTrade = function() {
+      self.sendPacket('trade_update', {'action': 'accept'});
    };
-   self.useGreatBall = function(target) {
-      var item = utilities.findBy(data.itemList, 'name', 'Great Ball');
-      (item && item.quantity > 0 && self.sendPacket('pokeball', {'t': target, 'i': item.id}));
+   self.tradeItem = function(name) {
+      var item = utilities.findBy(data.itemList, 'name', name);
+      (item && self.sendPacket('trade_update', {'action': 'item_add', 'pk': item.id}));
    };
-   self.usePokeBall = function(target) {
-      var item = utilities.findBy(data.itemList, 'name', 'Poke Ball');
-      (item && item.quantity > 0 && self.sendPacket('pokeball', {'t': target, 'i': item.id}));
+   self.tradePokemon = function(position) {
+      var pokemon = utilities.findBy(data.equippedPokemon, 'position', position);
+      (pokemon && self.sendPacket('trade_update', {'action': 'pokemon_add', 'pk': pokemon.id}));
    };
    self.withdrawPokemon = function(position) {
       var pokemon = utilities.findBy(data.equippedPokemon, 'position', position);
@@ -82,16 +68,6 @@ window.pokeApi = function(server, utilities, data, parser) {
       var pokemon = utilities.findBy(data.equippedPokemon, 'position', position);
       (pokemon && self.sendPacket('call', {'position': position, 'pk': pokemon.id, 'action': 'sendout'}));
    };
-   self.widthdrawEquippedPokemon = function() {
-      for ( var i = 0; i < data.equippedPokemon.length; i++ ) {
-         self.withdrawPokemon(data.equippedPokemon.position);
-      }
-   };
-   self.sendoutEquippedPokemon = function() {
-      for ( var i = 0; i < data.equippedPokemon.length; i++ ) {
-         self.sendoutPokemon(data.equippedPokemon.position);
-      }
-   };
    self.evolvePokemon = function(position) {
       var pokemon = utilities.findBy(data.equippedPokemon, 'position', position);
       (pokemon && self.sendPacket('evo', {'pk': pokemon.id, 'from': pokemon.monsterId, 'to': pokemon.monsterId + 1}));
@@ -99,6 +75,10 @@ window.pokeApi = function(server, utilities, data, parser) {
    self.changeSkill = function(position, index) {
       var pokemon = utilities.findBy(data.equippedPokemon, 'position', position);
       (pokemon && self.sendPacket('skill', {'pk': pokemon.id, 'index': index}));
+   };
+   self.usePokeBall = function(type, id) {
+      var item = utilities.findBy(data.itemList, 'name', type + ' Ball');
+      (item && item.quantity > 0 && --item.quantity && self.sendPacket('pokeball', {'t': id, 'i': item.id}));
    };
    utilities.interceptSocket(server, self.setSocket, parser.parsePacket);
 };
