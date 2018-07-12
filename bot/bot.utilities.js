a300d0ef7e5101c786eb1493fa56d066968eb6f2
jakemadness <j4kem4dness@live.com>
2018-07-12 08:56:24 +0100

    Update bot.utilities.js

--- bot/bot.utilities.js
+++ bot/bot.utilities.js
@@ -43,8 +43,6 @@ window.botUtilities = function() {
       request.send(post);
    };
    self.postRequest = function(url, post, callback) {
-      var cookies = document.cookie;
       self.httpRequest('POST', url, ['X-Requested-With', 'XMLHttpRequest', 'Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8'], post, callback);
-      document.cookie = cookies;
    };
 };
