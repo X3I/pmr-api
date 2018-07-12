8b537611c27dbf8834db366c3f1ec30be9957b1d
jakemadness <j4kem4dness@live.com>
2018-07-12 09:18:31 +0100

    Update script.js

--- bot/script.js
+++ bot/script.js
@@ -3,7 +3,6 @@ window.botScript = function(username, password, server, modifications) {
    var data      = new window.botData();
    var socket    = new window.botSocket(server, utilities, data);
    var api       = new window.botApi(utilities, data, socket);
-   utilities.blockCookies();
    api.login(username, password, function(token) {
       console.log('logged in!');
       api.authenticate(token, function() {
