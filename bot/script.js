e5a0987a0fd6f347639d6e910c5ce977810c2021
jakemadness <j4kem4dness@live.com>
2018-07-12 09:40:24 +0100

    Update script.js

--- bot/script.js
+++ bot/script.js
@@ -1,7 +1,13 @@
-window.botScript = function(server, modifications) {
+window.botScript = function(username, password, server, modifications) {
    var utilities = new window.botUtilities();
    var data      = new window.botData();
    var socket    = new window.botSocket(server, utilities, data);
    var api       = new window.botApi(utilities, data, socket);
-   modifications(utilities, data, socket, api);
+   api.login(username, password, function(token) {
+      console.log('bot logged in!' + token);
+      api.authenticate(token, function() {
+         console.log('bot authenticated!');
+         modifications(utilities, data, socket, api);
+      });
+   });
 };
