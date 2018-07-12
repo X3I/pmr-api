4172f054882634139e835c28f648700488ac4660
jakemadness <j4kem4dness@live.com>
2018-07-12 22:39:52 +0100

    Update poke.utilities.js

--- client/poke.utilities.js
+++ client/poke.utilities.js
@@ -8,6 +8,12 @@ window.pokeUtilities = function() {
       }
       return true;
    };
+   self.sumKeys = function(object, keys) {
+      for ( var sum = 0, i = 0; i < keys.length; i++ ) {
+         sum += object[keys[i]];
+      }
+      return sum;
+   };
    self.findBy = function(array, key, value) {
       for ( var i = 0; i < array.length; i++ ) {
          if ( array[i][key] == value ) {
