c9a571864a5b91faa6fbfbd393b0c89037a6ddf6
jakemadness <j4kem4dness@live.com>
2018-07-13 23:26:47 +0100

    Update bot.api.js

--- bot/bot.api.js
+++ bot/bot.api.js
@@ -9,6 +9,12 @@ window.botApi = function(utilities, data, socket) {
    self.useEmote = function(emote) {
       self.sendPacket('emote', {'style': emote});
    };
+   self.purchaseItem = function(token, id, quantity) {
+      self.sendPacket('purchase', {'token': token, 'purchases': [{'id': id, 'quantity': quantity}]});
+   };
+   self.sellLabPokemon = function(pokemonIds) {
+      self.sendPacket('lab', {'token': data.labToken, 'lab': pokemonIds});
+   };
    self.moveAvatar = function(x, y) {
       self.sendPacket('target', {'x': x, 'y': y});
    };
@@ -24,6 +30,15 @@ window.botApi = function(utilities, data, socket) {
    self.guildMessage = function(text) {
       self.sendPacket('message', {'chatroom': 'P', 'text': text});
    };
+   self.guildInvite = function(username) {
+      self.sendPacket('guildInvite', {'name': username});
+   };
+   self.guildUpdate = function(id, name, description, color) {
+      self.sendPacket('guildUpdate', {'guild_id': id, 'guild_name': name, 'guild_description': description, 'guild_color': color});
+   };
+   self.guildMoney = function(money) {
+      self.sendPacket('guildTransferMoney', {'money': money});
+   };
    self.tradeRequest = function(username) {
       self.sendPacket('trade_request', {'name': username});
    };
@@ -33,53 +48,49 @@ window.botApi = function(utilities, data, socket) {
    self.tradeMoney = function(amount) {
       self.sendPacket('trade_update', {'action': 'money', 'money': amount});
    };
-   self.tradePokemon = function(id) {
-      self.sendPacket('trade_update', {'action': 'pokemon_add', 'pk': id});
-   };
-   self.tradeItem = function(id) {
-      self.sendPacket('trade_update', {'action': 'item_add', 'pk': id});
-   };
    self.lockTrade = function() {
       self.sendPacket('trade_update', {'action': 'lock'});
    };
    self.acceptTrade = function() {
       self.sendPacket('trade_update', {'action': 'accept'});
    };
-   self.speakToNpc = function(npc) {
-      self.sendPacket('action', {'npc': npc});
+   self.tradeItem = function(name) {
+      var item = utilities.findBy(data.items, 'name', name);
+      (item && item.quantity > 0 && --item.quantity && self.sendPacket('trade_update', {'action': 'item_add', 'pk': item.id}));
    };
-   self.useMasterBall = function(target) {
-      self.sendPacket('pokeball', {'t': target, 'i': 1});
+   self.tradePokemon = function(position) {
+      var pokemon = utilities.findBy(data.equippedPokemon, 'position', position);
+      (pokemon && self.sendPacket('trade_update', {'action': 'pokemon_add', 'pk': pokemon.id}));
    };
-   self.useUltraBall = function(target) {
-      self.sendPacket('pokeball', {'t': target, 'i': 2});
+   self.sendoutPokemon = function(position) {
+      var pokemon = utilities.findBy(data.equippedPokemon, 'position', position);
+      (pokemon && self.sendPacket('call', {'position': position, 'pk': pokemon.id, 'action': 'sendout'}));
    };
-   self.useGreatBall = function(target) {
-      self.sendPacket('pokeball', {'t': target, 'i': 3});
+   self.withdrawPokemon = function(position) {
+      var pokemon = utilities.findBy(data.equippedPokemon, 'position', position);
+      (pokemon && self.sendPacket('call', {'position': position, 'pk': pokemon.id, 'action': 'withdraw'}));
    };
-   self.usePokeBall = function(target) {
-      self.sendPacket('pokeball', {'t': target, 'i': 4});
+   self.evolvePokemon = function(position) {
+      var pokemon = utilities.findBy(data.equippedPokemon, 'position', position);
+      (pokemon && self.sendPacket('evo', {'pk': pokemon.id, 'from': pokemon.monsterId, 'to': pokemon.monsterId + 1}));
    };
-   self.withdrawPokemon = function(position, id) {
-      self.sendPacket('call', {'position': position, 'pk': id, 'action': 'withdraw'});
+   self.changePokemonAttack = function(position, attackNumber) {
+      var pokemon = utilities.findBy(data.equippedPokemon, 'position', position);
+      (pokemon && self.sendPacket('skill', {'pk': pokemon.id, 'index': attackNumber - 1}));
    };
-   self.sendoutPokemon = function(position, id) {
-      self.sendPacket('call', {'position': position, 'pk': id, 'action': 'sendout'});
+   self.usePokeBall = function(name, id) {
+      var item = utilities.findBy(data.items, 'name', name);
+      (item && item.quantity > 0 && --item.quantity && self.sendPacket('pokeball', {'t': id, 'i': item.id}));
    };
-   self.evolvePokemon = function(id, from, to) {
-      self.sendPacket('evo', {'pk': id, 'from': from, 'to': to});
+   self.useItem = function(position, name) {
+      var pokemon = utilities.findBy(data.equippedPokemon, 'position', position);
+      var item    = utilities.findBy(data.items, 'name', name);
+      (pokemon && item && item.quantity > 0 && --item.quantity && self.sendPacket('item', {'t': 'p' + pokemon.id,'i': item.id}));
    };
-   self.changeSkill = function(id, index) {
-      self.sendPacket('skill', {'pk': id, 'index': index});
-   };
-   self.guildInvite = function(username) {
-      self.sendPacket('guildInvite', {'name': username});
-   };
-   self.guildUpdate = function(id, name, description, color) {
-      self.sendPacket('guildUpdate', {'guild_id': id, 'guild_name': name, 'guild_description': description, 'guild_color': color});
-   };
-   self.guildUpdate = function(money) {
-      self.sendPacket('guildTransferMoney', {'money': money});
+   self.revivePokemon = function(position, name) {
+      var pokemon = utilities.findBy(data.equippedPokemon, 'position', position);
+      var item    = utilities.findBy(data.items, 'name', name);
+      (pokemon && item && item.quantity > 0 && --item.quantity && self.sendPacket('revive', {'t': pokemon.id,'i': item.id}));
    };
    self.register = function(username, password, email, pokemonId, success, error) {
       utilities.postRequest('/ajax/register', utilities.queryString({
@@ -112,6 +123,11 @@ window.botApi = function(utilities, data, socket) {
          'password': password
       }), function(data) {
          ((match = data.match(/token\s*=\s*.([0-9a-z]*)/i)) ? (success && success(match['1'])) : (error && error()));
+      });
+   };
+   self.logout = function(success, error) {
+      utilities.getRequest('/logout/', function(data) {
+         (data.match(/logged\sout/i) ? (success && success()) : (error && error()));
       });
    };
 };
