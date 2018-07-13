69ebbd4098515d6aa8d2db46b9af1987890bb778
jakemadness <j4kem4dness@live.com>
2018-07-13 23:47:59 +0100

    Update bot.socket.js

--- bot/bot.socket.js
+++ bot/bot.socket.js
@@ -1,28 +1,3 @@
-// ==UserScript==
-// @name         PMR BOT API
-// @namespace    PRM BOT API
-// @version      1
-// @description  Bot version of PMR API
-// @author       jakemadness
-// @license      GPL
-// @match        *://pokemonrise.com/*
-// @require      https://cdn.rawgit.com/jakemadness/pmr-api/47f8ea819bf541207dc8ca90a7b2734c435644e7/bot/bot.utilities.js
-// @require      https://cdn.rawgit.com/jakemadness/pmr-api/89d8ffdbca287eb891d3a480f81b1403f421a99a/bot/bot.data.js
-// @require      https://cdn.rawgit.com/jakemadness/pmr-api/a401cdb6a2ad13fb8f6830d4d3736e092daa9b2c/bot/bot.parser.js
-// @require      https://cdn.rawgit.com/jakemadness/pmr-api/d3adb75bd410da5a8e6cbcc13a7c124c4e048019/bot/bot.socket.js
-// @require      https://cdn.rawgit.com/jakemadness/pmr-api/fe489b1c371aadfeb938c0d31faeb36165f005a3/bot/bot.api.js
-// @require      https://cdn.rawgit.com/jakemadness/pmr-api/3e29268dd65755391d379a27663a504fab4ee39f/bot/script.js
-// @run-at       document-end
-// @grant        none
-// ==/UserScript==
-var modifications = function(utilities, data, socket, api) {
-   api.privateMessage('XD', 'hello world!');
-};
-window.bot = new window.botScript('ecchi', 'Password123', 'wss://www.pokemonrise.com:8081/game', modifications);
-
-
-
-
 window.botSocket = function(server, parser) {
    var self       = this;
    self.ws        = false;
