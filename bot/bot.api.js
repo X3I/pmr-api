5fad77b812cc8bdfca33612855e1bf5b06a71de8
jakemadness <j4kem4dness@live.com>
2018-07-13 23:58:21 +0100

    Update bot.api.js

--- bot/bot.api.js
+++ bot/bot.api.js
@@ -4,7 +4,7 @@ window.botApi = function(utilities, data, socket) {
       socket.sendPacket({'a': action, 'p': packet});
    };
    self.authenticate = function(token, callback) {
-      socket.sendPacket('auth', {'token': token}, callback);
+      socket.sendPacket({{'a': 'auth', 'p': {'token': token}}, callback);
    };
    self.useEmote = function(emote) {
       self.sendPacket('emote', {'style': emote});
