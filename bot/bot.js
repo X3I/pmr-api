03bcacfdb64d3e6eaad2baad82b671515c2ecb19
jakemadness <j4kem4dness@live.com>
2018-07-14 20:44:30 +0100

    Update bot.js

--- bot/bot.js
+++ bot/bot.js
@@ -2,11 +2,11 @@
    'use strict';
    botModules.create('bot', function(server, username, password, callback) { 
       var self       = this;
-      self.utilities = modules.require('utilities', []);
-      self.data      = modules.require('data',      []);
-      self.parser    = modules.require('parser',    [self.utilities, self.data]);
-      self.socket    = modules.require('socket',    [server, self.parser]);
-      self.api       = modules.require('api',       [self.utilities, self.data, self.socket]);
+      self.utilities = botModules.require('utilities', []);
+      self.data      = botModules.require('data',      []);
+      self.parser    = botModules.require('parser',    [self.utilities, self.data]);
+      self.socket    = botModules.require('socket',    [server, self.parser]);
+      self.api       = botModules.require('api',       [self.utilities, self.data, self.socket]);
       self.socket.open(function() {
          console.log('socket opened!');
          self.api.login(username, password, function() {
