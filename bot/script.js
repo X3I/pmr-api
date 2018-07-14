6eb00421a89ce79889a81fc6d327f93027f6d329
jakemadness <j4kem4dness@live.com>
2018-07-14 01:47:33 +0100

    Update script.js

--- bot/script.js
+++ bot/script.js
@@ -5,12 +5,11 @@ window.botScript = function(username, password, server, modifications) {
    self.parser    = new window.botParser(self.utilities, self.data);
    self.socket    = new window.botSocket(server, self.parser);
    self.api       = new window.botApi(self.utilities, self.data, self.socket);
-   self.sessionId = self.utilities.getCookie('PHPSESSID');
-   console.log(self.sessionId);
-   self.utilities.deleteCookie('PHPSESSID');
+   self.session   = self.utilities.getCookie('session');
+   (self.session && self.utilities.deleteCookie('PHPSESSID'));
    self.socket.socketReady(function() {
       self.api.login(username, password, function(token) {
-         self.utilities.setCookie('PHPSESSID', self.sessionId);
+         (self.session && self.utilities.setCookie('PHPSESSID', self.sessionId));
          console.log('bot logged in!');
          self.api.authenticate(token, function() {
             console.log('bot authenticated!');
