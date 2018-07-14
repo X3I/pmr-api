22b2fe2a431b36708d8664d410514bfb7198d95c
jakemadness <j4kem4dness@live.com>
2018-07-14 01:44:24 +0100

    Update script.js

--- client/script.js
+++ client/script.js
@@ -4,6 +4,7 @@ window.pokeScript = function(server, modifications) {
    self.data      = new window.pokeData();
    self.parser    = new window.pokeParser(self.utilities, self.data);
    self.api       = new window.pokeApi(server, self.utilities, self.data, self.parser);
+   self.utilities.setCookie('session', self.utilities.getCookie('PHPSESSID'));
    self.api.socketReady(function() {
       modifications(self);
    });
