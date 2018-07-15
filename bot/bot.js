f485540776791e8683454d08388d4e371d6bcf81
jakemadness <j4kem4dness@live.com>
2018-07-15 08:12:41 +0100

    Update bot.js

--- bot/bot.js
+++ bot/bot.js
@@ -1,20 +1,27 @@
 (function() {
    'use strict';
-   botModules.create('bot', function(server, username, password, callback) { 
+   botModules.create('bot', function(server, username, password, callback) {
+
       var self       = this;
       self.utilities = botModules.require('utilities', []);
       self.data      = botModules.require('data',      []);
       self.parser    = botModules.require('parser',    [self.utilities, self.data]);
       self.socket    = botModules.require('socket',    [server, self.parser]);
       self.api       = botModules.require('api',       [self.utilities, self.data, self.socket]);
-      self.socket.open(function() {
+
+      var open = function() {
          console.log('socket opened!');
          self.api.login(username, password, function() {
             console.log('logged in!');
             callback(self);
          });
-      }, function() {
+      };
+      var close = function() {
          console.log('socket closed!');
-      });
+         self.socket.open(open, close);
+      };
+
+      self.socket.open(open, close);
+
    });
 })();
