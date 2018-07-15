ee49720685fdf68d32cdb59f8f2ab34c21502be5
jakemadness <j4kem4dness@live.com>
2018-07-15 17:23:48 +0100

    Update bot.modules.js

--- bot/bot.modules.js
+++ bot/bot.modules.js
@@ -3,17 +3,13 @@
    var modules = function() {
       var self     = this;
       self.modules = [];
-      self.loaded  = [];
       self.create = function(name, module) {
          self.modules.push(name, module);
       };
       self.require = function(name, dependencies) {
-         if ( self.loaded.indexOf(name) < 0 ) {
-            self.loaded.push(name, new (function() {
-               return self.modules[self.modules.indexOf(name) + 1].apply(this, dependencies || []);
-            })());
-         }
-         return self.loaded[self.loaded.indexOf(name) + 1];
+         return new (function() {
+            return self.modules[self.modules.indexOf(name) + 1].apply(this, dependencies || []);
+         });
       };
    };
    window.botModules = new modules();
