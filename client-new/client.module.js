d94b0d105f5c5ae41a7a3b92ac72492aeb3ddbe6
jakemadness <j4kem4dness@live.com>
2018-07-15 20:48:15 +0100

    Create client.module.js

---
+++ client-new/client.module.js
@@ -0,0 +1,13 @@
+(function() {
+   'use strict';
+   window.modules.create('client', function(server, callback) {
+      var self       = this;
+      self.utilities = window.modules.require('utilities', []);
+      self.data      = window.modules.require('data', []);
+      self.parser    = window.modules.require('parser', [self.utilities, self.data]);
+      self.api       = window.modules.require('api', [server, self.utilities, self.data, self.parser]);
+      self.api.socketReady(function() {
+         callback(self);
+      });
+   });
+})();
