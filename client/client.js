442605b1c4500708dabb9de83b0b393ce5078714
jakemadness <j4kem4dness@live.com>
2018-07-16 09:57:46 +0100

    Update client.js

--- client/client.js
+++ client/client.js
@@ -6,6 +6,7 @@
       self.data      = window.modules.require('data',      []);
       self.parser    = window.modules.require('parser',    [self.utilities, self.data]);
       self.api       = window.modules.require('api',       [server, self.utilities, self.data, self.parser]);
+      console.log(callback);
       self.utilities.socketReady(self.api.socket, function() {
          callback(self);
       });
