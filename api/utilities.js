198b000a8a22ad4b68d66b61df0d7d76e9544253
jakemadness <j4kem4dness@live.com>
2018-07-22 22:24:26 +0100

    Create utilities.js

---
+++ api/utilities.js
@@ -0,0 +1,73 @@
+(function() {
+   'use strict';
+   window.modules.create('utilities', function() {
+      var self          = this;
+      self.keysInObject = function(object, keys) {
+         for ( var i = 0, length = keys.length; i < length; i++ ) {
+            if ( !(keys[i] in object) ) {
+               return false;
+            }
+         }
+         return true;
+      };
+      self.sumKeys = function(object, keys) {
+         for ( var i = 0, length = keys.length, sum = 0; i < length; i++ ) {
+            sum += object[keys[i]];
+         }
+         return sum;
+      };
+      self.sortBy = function(array, key, descending) {
+         return array.sort(function(a, b) {
+            return (descending ? b[key] - a[key] : a[key] - b[key]);
+         });
+      };
+      self.findBy = function(array, key, value) {
+         for ( var i = 0, length = array.length; i < length; i++ ) {
+            if ( array[i][key] == value ) {
+               return array[i];
+            }
+         }
+      };
+      self.deleteBy = function(array, key, value) {
+         for ( var i = 0, length = array.length; i < length; i++ ) {
+            if ( array[i][key] == value ) {
+               array.splice(i, 1);
+               break;
+            }
+         }
+      };
+      self.createElement = function(tag, attributes, value, onclick) {
+         var element       = document.createElement(tag);
+         element.innerHTML = value   || '';
+         element.onclick   = onclick || null;
+         for ( var i = 0, length = attributes.length; i < length; i += 2 ) {
+            element.setAttribute(attributes[i], attributes[i + 1]);
+         }
+         return element;
+      };
+      self.appendChildren = function(element, children) {
+         for ( var i = 0, length = children.length; i < length; i++ ) {
+            element.appendChild(children[i]);
+         }
+      };
+      self.removeChildren = function(element) {
+         while ( element.hasChildNodes() ) {
+            element.removeChild(element.lastChild);
+         }
+      };
+      self.interceptSocket = function(url, set, parse) {
+         var websocket    = window.WebSocket;
+         window.WebSocket = function(server) {
+            var socket = new websocket(server);
+            if ( url == server ) {
+               set(socket);
+               socket.addEventListener('message', function(event) {
+                  parse(event.data);
+               });
+            }
+            return socket;
+         };
+         window.WebSocket.prototype = websocket.prototype;
+      };
+   });
+})();
